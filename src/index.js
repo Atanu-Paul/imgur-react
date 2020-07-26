98f098406b75143454606b4cac46e2dba106ba96
Atanu Paul <38816880+Atanu-Paul-au6@users.noreply.github.com>
2020-07-26 13:07:36 +0530

    new files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import AppRouter from './router/AppRouter';
+import store from './store/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux'
-import store from './redux/store';
-// import 'antd/dist/antd.css'
+import './styles.scss';
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <AppRouter />
   </Provider>,
   document.getElementById('root')
 );
-
